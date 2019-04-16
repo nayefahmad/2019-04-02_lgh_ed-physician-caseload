df9be33327f274e0c32b88ce151ad07f0265c0ce
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-04-16 16:43:47 -0700

    function to take map across doctor codes, each time filtering for all patient records where that doctor was involved

--- src/filter-by-doc_function.R
+++ src/filter-by-doc_function.R
@@ -10,9 +10,36 @@
 #*****************************************************************************
 
 
-# define function: 
+# define function: --------
 filter_rows_by_doc <- 
-  function() {
-    print("Hi")
+  function(doc_code, df) {
+    
+    # input: dataframe of patient-level records. 
+    
+    # output: filter rows to include only those where the doctor we're
+    # interested in ("doc_code") is involved as Dr1, or D2, or ... Dr8
+    
+    
+    df_filtered <- 
+      df %>% 
+      filter(DR1DoctorCode == doc_code | 
+               DR2DoctorCode == doc_code |
+               DR3DoctorCode == doc_code | 
+               DR4DoctorCode == doc_code |
+               DR5DoctorCode == doc_code |
+               DR6DoctorCode == doc_code |
+               DR7DoctorCode == doc_code |
+               DR8DoctorCode == doc_code
+      )
+    
+    return(df_filtered)
     
   }
+
+
+
+# test function: --------
+# filter_rows_by_doc("26650", 
+#                    df1.ed_data) %>% 
+#   select(starts_with("DR")) %>% 
+#   View("test")
